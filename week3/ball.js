f8397d6f82ebeeaaa313b082e83411a8dfc392b3
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-02-04 10:04:02 -0500

    Add files via upload

--- week3/ball.js
+++ week3/ball.js
@@ -1,19 +1,27 @@
-// this function makes a ball with position and velocity set
-
 function makeBall(xcoord, ycoord, color, velx = 0, vely = 0, fixed = 0) {
-  ball = document.createElement("div");
-  ball.style.backgroundColor = color;
-  ball.className = "ball";
-  ball.style.height = ball.style.width = size;
-  ball.style.top = ycoord;
-  ball.style.left = xcoord;
-  document.body.appendChild(ball);
-  if (!fixed) {
-    // only free balls will be updated
-    balls.push(ball);
-    x.push(xcoord);
-    y.push(ycoord);
-    velocity_x.push(velx);
-    velocity_y.push(vely);
+    ball = document.createElement("div");
+    ball.style.backgroundColor = color;
+    ball.className = "ball";
+    ball.style.height = ball.style.width = size;
+    ball.style.top = ycoord;
+    ball.style.left = xcoord;
+    document.body.appendChild(ball);
+    if (!fixed) {
+      // only free balls will be updated
+      balls.push(ball);
+      x.push(xcoord);
+      y.push(ycoord);
+      velocity_x.push(velx);
+      velocity_y.push(vely);
+    }
   }
-}
+  function makeBox(xcoord, ycoord, color){
+    box = document.createElement('div')
+    box.style.backgroundColor = `${color}`;
+    box.className = `box`;
+    box.style.height = size
+    box.style.width = size;
+    box.style.top = ycoord;
+    box.style.left = xcoord;
+    document.getElementById('gameArea').appendChild(box);
+  }
