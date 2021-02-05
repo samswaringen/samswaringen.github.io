1305c737246e505be4ed106fc2eb4f5555c60e7b
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-02-05 15:34:37 -0500

    changed my landing website. 1st edition

---
+++ script.js
@@ -0,0 +1,85 @@
+
+for(let i = 0; i<11; i++){
+    let targetDiv = document.getElementById(`link${i}`);
+    targetDiv.onmouseover = targetDiv.onmouseout = handleFile;
+}
+
+function handleFile(event){
+    let name = event.target.id;
+    let number = Number(name.split('k').pop());
+    if(number <= 9){
+        file = document.getElementById(`file${number}`);
+        fileTop = parseInt(file.style.top);
+    }else {
+        file = document.getElementById(`file9`);
+        fileTop = parseInt(file.style.top); 
+    }
+    let namedDiv =document.getElementById(`${name}`);
+    let lastLink = document.getElementById('link9'); 
+    let height = parseInt(namedDiv.style.height);
+    let top = parseInt(namedDiv.style.top);
+    let expandTop = parseInt(lastLink.style.top);
+    let website = document.getElementById(`website${number}`);
+    fixNamedBorder = () => {
+        namedDiv.style.borderBottom = 'none';
+        namedDiv.style.borderRadius = '5px 5px 0px 0px';
+        namedDiv.style.lineHeight = '29px';
+        
+    }
+    fixLastBorder = () => {
+        lastLink.style.borderBottom = '2px solid black';
+        lastLink.style.borderRadius = '5px';
+    }
+    if(event.type == 'mouseover'){
+        if(number === 8){
+            height += 41 ;
+            namedDiv.style.borderBottom = '5px dotted white';
+            namedDiv.style.borderRadius = '5px 5px 5px 5px';
+            namedDiv.style.lineHeight = '68px';
+        } if(number === 9){
+            fixLastBorder();
+            expandTop -= 50;
+        }if(number === 10){
+            fixLastBorder();
+            height += 50;
+            expandTop -= 50;
+        }else if(number<8){
+             height += 41;
+             namedDiv.style.fontSize = '25px'
+             namedDiv.style.lineHeight = '68px';
+             namedDiv.style.borderRadius = '5px';
+             namedDiv.style.borderBottom = '2px solid black';
+        }
+     lastLink.style.top = expandTop + 'px';  
+     namedDiv.style.height = height + 'px';
+     top -= 50;
+     namedDiv.style.top = top + 'px';
+     fileTop -= 50;
+     file.style.top = fileTop + 'px';
+     document.getElementById('newFrame').src = website; 
+}
+    if(event.type == 'mouseout'){
+        if(number === 8){
+            height -=41;
+            fixNamedBorder();
+        }if(number === 9){
+            fixLastBorder();
+            expandTop +=50;
+        }if(number === 10){
+            fixLastBorder();
+            height -=50;
+            expandTop +=50;
+        }else if (number < 8) {
+             height -= 41;
+             namedDiv.style.fontSize = '18px';
+             fixNamedBorder();
+             fixLastBorder();
+        }
+      lastLink.style.top = expandTop + 'px'; 
+      namedDiv.style.height = height + 'px';
+      top += 50;
+      namedDiv.style.top = top + 'px';
+      fileTop += 50;
+      file.style.top = fileTop + 'px'; 
+}
+}
