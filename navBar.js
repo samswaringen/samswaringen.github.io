1bbd2917ce2f7e8c76036f278ce4ddce3c96674b
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-03-14 15:03:02 -0400

    Add files via upload

--- navBar.js
+++ navBar.js
@@ -2,20 +2,20 @@ addNavBar = ()=>{
     let navDiv = document.getElementById('navBarDiv');
     navDiv.innerHTML = `<nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="https://samswaringen.github.io">Navbar</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="https://samswaringen.github.io">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href='default%20page/aboutme.html'>About Me</a>
+            <a class="nav-link" href='https://samswaringen.github.io/default%20page/aboutme.html'>About Me</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href='default%20page/links.html'>Links</a>
+            <a class="nav-link" href='https://samswaringen.github.io/default%20page/links.html'>Links</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -24,7 +24,8 @@ addNavBar = ()=>{
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
               <li><a class="dropdown-item" href='https://samswaringen.github.io/week3'>Random Painter</a></li>
               <li><a class="dropdown-item" href='https://samswaringen.github.io/week4'>PacMan Eraser</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="https://samswaringen.github.io/bustracker">Bus Tracker</a></li>
+              <li><a class="dropdown-item" href="https://samswaringen.github.io/challenges">Coding Challenges</a></li>
             </ul>
           </li>
         </ul>
