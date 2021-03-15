10cc2581f2429cf4d9334d9b34fc0bdd404db91f
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-03-15 13:35:30 -0400

    changed wording of navbar to where to?

--- navBar.js
+++ navBar.js
@@ -2,7 +2,7 @@ addNavBar = ()=>{
     let navDiv = document.getElementById('navBarDiv');
     navDiv.innerHTML = `<nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="https://samswaringen.github.io">Navbar</a>
+      <a class="navbar-brand" href="https://samswaringen.github.io">Where to?</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
