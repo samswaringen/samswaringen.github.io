4c4ef0ec736298812399875e67ba18ee3f339f71
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-03-18 14:46:34 -0400

    updated styling

--- challenges/popover.js
+++ challenges/popover.js
@@ -2,5 +2,9 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTrigger) {
   return new bootstrap.Popover(popoverTrigger)
 })
-var popover = new bootstrap.Popover(popoverList);
 
+var focusEl = document.getElementById('likes-input');
+console.log(focusEl);
+if (focusEl === document.activeElement){
+    focusEl.value = '';
+}
