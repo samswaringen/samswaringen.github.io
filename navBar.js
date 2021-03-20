403a9dedbf263c1e5cc54c7ac1e5351317c97edf
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-03-20 12:49:08 -0400

    added links to github for projects

--- navBar.js
+++ navBar.js
@@ -3,7 +3,7 @@ addNavBar = ()=>{
     navDiv.innerHTML = `<nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="https://samswaringen.github.io">Where to?</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" onfocus = 'fixZInd()' aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
@@ -21,13 +21,25 @@ addNavBar = ()=>{
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Projects
             </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <ul class="dropdown-menu" id = 'dropdown' aria-labelledby="navbarDropdownMenuLink">
               <li><a class="dropdown-item" href='https://samswaringen.github.io/week3'>Random Painter</a></li>
               <li><a class="dropdown-item" href='https://samswaringen.github.io/week4'>PacMan Eraser</a></li>
               <li><a class="dropdown-item" href="https://samswaringen.github.io/bustracker">Bus Tracker</a></li>
               <li><a class="dropdown-item" href="https://samswaringen.github.io/challenges">Coding Challenges</a></li>
               <li><a class="dropdown-item" href="https://samswaringen.github.io/eyes">EYES</a></li>
             </ul>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            GitHub Links
+          </a>
+            <ul class="dropdown-menu" id = 'dropdown' aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href='https://github.com/samswaringen/samswaringen.github.io/tree/main/week3'>Random Painter Github</a></li>
+              <li><a class="dropdown-item" href='https://github.com/samswaringen/samswaringen.github.io/tree/main/week4'>PacMan Eraser Github</a></li>
+              <li><a class="dropdown-item" href="https://github.com/samswaringen/samswaringen.github.io/tree/main/bustracker">Bus Tracker GitHub</a></li>
+              <li><a class="dropdown-item" href="https://github.com/samswaringen/samswaringen.github.io/tree/main/challenges">Coding Challenges GitHub</a></li>
+              <li><a class="dropdown-item" href="https://github.com/samswaringen/samswaringen.github.io/tree/main/eyes">EYES GitHub</a></li>
+            </ul>
+            </li>
           </li>
         </ul>
       </div>
