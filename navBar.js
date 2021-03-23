79808e98cb530796d106ac5a18ef9d56146fe456
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-03-23 08:49:32 -0400

    Add files via upload

--- navBar.js
+++ navBar.js
@@ -19,23 +19,23 @@ addNavBar = ()=>{
               Projects
             </a>
             <ul class="dropdown-menu" id = 'dropdownPro' aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" href='https://samswaringen.github.io/week3'>Random Painter</a></li>
-              <li><a class="dropdown-item" href='https://samswaringen.github.io/week4'>PacMan Eraser</a></li>
-              <li><a class="dropdown-item" href="https://samswaringen.github.io/bustracker">Bus Tracker</a></li>
-              <li><a class="dropdown-item" href="https://samswaringen.github.io/challenges">Coding Challenges</a></li>
-              <li><a class="dropdown-item" href="https://samswaringen.github.io/eyes">EYES</a></li>
+              <li><a class="dropdown-item" id = 'painter' href='https://samswaringen.github.io/week3'>Random Painter</a></li>
+              <li><a class="dropdown-item" id = 'eraser' href='https://samswaringen.github.io/week4'>PacMan Eraser</a></li>
+              <li><a class="dropdown-item" id = 'tracker' href="https://samswaringen.github.io/bustracker">Bus Tracker</a></li>
+              <li><a class="dropdown-item" id = 'challenges' href="https://samswaringen.github.io/challenges">Coding Challenges</a></li>
+              <li><a class="dropdown-item" id = 'eyes' href="https://samswaringen.github.io/eyes">EYES</a></li>
             </ul>
           <li class="nav-item dropdown">
             <a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             GitHub Links
           </a>
             <ul class="dropdown-menu" id = 'dropdownGit' aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" href='https://github.com/samswaringen/samswaringen.github.io/tree/main/portfolio%20webpage'>Project Portfolio GitHub</a></li>
-              <li><a class="dropdown-item" href='https://github.com/samswaringen/samswaringen.github.io/tree/main/week3'>Random Painter Github</a></li>
-              <li><a class="dropdown-item" href='https://github.com/samswaringen/samswaringen.github.io/tree/main/week4'>PacMan Eraser Github</a></li>
-              <li><a class="dropdown-item" href="https://github.com/samswaringen/samswaringen.github.io/tree/main/bustracker">Bus Tracker GitHub</a></li>
-              <li><a class="dropdown-item" href="https://github.com/samswaringen/samswaringen.github.io/tree/main/challenges">Coding Challenges GitHub</a></li>
-              <li><a class="dropdown-item" href="https://github.com/samswaringen/samswaringen.github.io/tree/main/eyes">EYES GitHub</a></li>
+              <li><a class="dropdown-item" id = 'portfolio-git' href='https://github.com/samswaringen/samswaringen.github.io/tree/main/portfolio%20webpage'>Project Portfolio GitHub</a></li>
+              <li><a class="dropdown-item" id = 'painter-git' href='https://github.com/samswaringen/samswaringen.github.io/tree/main/week3'>Random Painter Github</a></li>
+              <li><a class="dropdown-item" id = 'eraser-git' href='https://github.com/samswaringen/samswaringen.github.io/tree/main/week4'>PacMan Eraser Github</a></li>
+              <li><a class="dropdown-item" id = 'tracker-git' href="https://github.com/samswaringen/samswaringen.github.io/tree/main/bustracker">Bus Tracker GitHub</a></li>
+              <li><a class="dropdown-item" id = 'challenges-git' href="https://github.com/samswaringen/samswaringen.github.io/tree/main/challenges">Coding Challenges GitHub</a></li>
+              <li><a class="dropdown-item" id = 'eyes-git' href="https://github.com/samswaringen/samswaringen.github.io/tree/main/eyes">EYES GitHub</a></li>
             </ul>
             </li>
           </li>
