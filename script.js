8625ff43d922b6285627c2f0f90e48a355de55b1
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-03-22 21:52:13 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-setTimeout(()=>{
-    document.getElementById('iframe').src = 'https://www.youtube.com/embed/?listType=playlist&list=PLs1pmxBvOzVpzouqMoz3oZiTqH6ADJ1b5'
-},1500);
+
+document.getElementById('leftDiv').addEventListener('mouseover', ()=>{
+    document.getElementById('board').style.transform = 'rotate(20deg)';
+    document.getElementById('board').style.transform = 'rotate(10deg)';
+    document.getElementById('board').style.transform = 'rotate(20deg)';
+})
+document.getElementById('leftDiv').addEventListener('mouseleave', ()=>{
+    document.getElementById('board').style.transform = 'rotate(0deg)';
+})
+document.getElementById('rightDiv').addEventListener('mouseover', ()=>{
+    document.getElementById('aboutme').style.transform = 'rotate(-20deg)';
+})
+document.getElementById('rightDiv').addEventListener('mouseleave', ()=>{
+    document.getElementById('aboutme').style.transform = 'rotate(0deg)';
+})
