eaa39ad7b06b7f1f864c7ab7c30b6d611e5d5dda
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-05-22 15:17:32 -0400

    updated css, js, and made single page

---
+++ atmApp/precache-manifest.b0cd70791739859f2a2f0906667c2eb5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b3711a076829decda7",
+    "url": "/atmApp/static/css/main.6c1f4a60.chunk.css"
+  },
+  {
+    "revision": "b8b3711a076829decda7",
+    "url": "/atmApp/static/js/main.f599b402.chunk.js"
+  },
+  {
+    "revision": "b5ad46a68a31ac149bdd",
+    "url": "/atmApp/static/js/runtime~main.1e58b22d.js"
+  },
+  {
+    "revision": "1b4e8846f946da029892",
+    "url": "/atmApp/static/js/2.2359fdfb.chunk.js"
+  },
+  {
+    "revision": "143b8c3b42cea7384b99",
+    "url": "/atmApp/static/js/3.58f6af48.chunk.js"
+  },
+  {
+    "revision": "7ccd322d1657513306fa36034bff6397",
+    "url": "/atmApp/static/media/temple.7ccd322d.png"
+  },
+  {
+    "revision": "4c13a36d54aeff49a093f568275a81fc",
+    "url": "/atmApp/static/media/marble.4c13a36d.jpg"
+  },
+  {
+    "revision": "15068cc4383fdc2a700e427ec8d2bec8",
+    "url": "/atmApp/static/media/DollarBill.15068cc4.ttf"
+  },
+  {
+    "revision": "c0fdc96a57afca89ee8e3aa6c41485ec",
+    "url": "/atmApp/static/media/UnitedStates.c0fdc96a.ttf"
+  },
+  {
+    "revision": "2f4482a3a4bd6c4f76054a461269e01a",
+    "url": "/atmApp/static/media/Silicone.2f4482a3.ttf"
+  },
+  {
+    "revision": "579223f9b31edfa5ea9756fc46186ff1",
+    "url": "/atmApp/static/media/SiliconeHollow.579223f9.ttf"
+  },
+  {
+    "revision": "d85fb3a9ae9c537064416a68f19ed1b5",
+    "url": "/atmApp/static/media/SiliconeFilled.d85fb3a9.ttf"
+  },
+  {
+    "revision": "b6967de302b2a1bd3e834e8e0f26e484",
+    "url": "/atmApp/static/media/Dalek.b6967de3.ttf"
+  },
+  {
+    "revision": "095bbb2195ef9038702aa7d28405331a",
+    "url": "/atmApp/static/media/Showboat.095bbb21.ttf"
+  },
+  {
+    "revision": "8509e4123597ca94fc13c55228c32ced",
+    "url": "/atmApp/index.html"
+  }
+];
